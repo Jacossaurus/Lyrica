ec6337b2404e237e25166ea9486b0122cdb630a6
Jacossaurus <kingjacob11@icloud.com>
2023-01-07 14:54:39 -0600

    fixes

--- src/main.ts
+++ src/main.ts
@@ -3,71 +3,70 @@ import { config } from "dotenv";
 import { deleteReply } from "./utils/deleteReply";
 import { getCommands } from "./utils/getCommands";
 import { getInteractions } from "./utils/getInteractions";
-import MusicService from "./services/musicService";
 
 config();
 
 const client = new Client({
-	intents: [],
+    intents: [],
 });
 
 async function init() {
-	const commands = await getCommands();
-	const interactions = await getInteractions();
+    const commands = await getCommands();
+    const interactions = await getInteractions();
 
-	client.on(Events.InteractionCreate, async (interaction) => {
-		if (interaction.isChatInputCommand()) {
-			const command = commands.get(interaction.commandName);
+    client.on(Events.InteractionCreate, async (interaction) => {
+        if (interaction.isChatInputCommand()) {
+            const command = commands.get(interaction.commandName);
 
-			if (command) {
-				await command.execute(interaction);
-			} else {
-				await interaction.reply(`Failed to find command.`);
+            if (command) {
+                await command.execute(interaction);
+            } else {
+                await interaction.reply(`Failed to find command.`);
 
-				deleteReply(interaction);
-			}
-		} else if (interaction.isAutocomplete()) {
-			const command = commands.get(interaction.commandName);
+                deleteReply(interaction);
+            }
+        } else if (interaction.isAutocomplete()) {
+            const command = commands.get(interaction.commandName);
 
-			if (command && command.autocomplete !== undefined) {
-				try {
-					await command.autocomplete(interaction);
-				} catch (error) {
-					console.error(
-						`Failed to run autocomplete for command ${interaction.commandName}. ${error}`
-					);
-				}
-			} else {
-				console.error(
-					`Failed to find command or command did not have autocomplete. Command: ${interaction.commandName}`
-				);
-			}
-		} else if (interaction.isStringSelectMenu() || interaction.isButton()) {
-			const interactionCallback = interactions.get(interaction.customId);
+            if (command && command.autocomplete !== undefined) {
+                try {
+                    await command.autocomplete(interaction);
+                } catch (error) {
+                    console.error(
+                        `Failed to run autocomplete for command ${interaction.commandName}. ${error}`
+                    );
+                }
+            } else {
+                console.error(
+                    `Failed to find command or command did not have autocomplete. Command: ${interaction.commandName}`
+                );
+            }
+        } else if (interaction.isStringSelectMenu() || interaction.isButton()) {
+            const interactionCallback = interactions.get(interaction.customId);
 
-			if (interactionCallback !== undefined) {
-				await interactionCallback(interaction);
-			} else {
-				await interaction.reply(
-					`Failed to find interaction from Custom ID (${interaction.customId})`
-				);
+            if (interactionCallback !== undefined) {
+                await interactionCallback(interaction);
+            } else {
+                await interaction.reply(
+                    `Failed to find interaction from Custom ID (${interaction.customId})`
+                );
 
-				deleteReply(interaction);
-			}
-		}
-	});
+                deleteReply(interaction);
+            }
+        }
+    });
 
-	client.once("ready", () => {
-		console.log(`${client.user.tag} is online!`);
+    client.once("ready", () => {
+        console.log(`${client.user.tag} is online!`);
 
-		client.user.setActivity("you.", {
-			type: ActivityType.Listening,
-		});
-	});
+        client.user.setActivity("you.", {
+            type: ActivityType.Listening,
+        });
+    });
 
-	client.login(process.env.DISCORD_TOKEN);
+    client.login(process.env.DISCORD_TOKEN);
 }
 
 init();
 
-process.on("uncaughtException", (err) => console.error(err));
+process.on("uncaughtException", (err) => console.log(err));
--- src/services/musicService.ts
+++ src/services/musicService.ts
@@ -12,158 +12,160 @@ const MUSIC_API_BASE = "https://api.musixmatch.com/ws/1.1";
 const CACHE_EXPIRY = 7 * 24 * 60 * 60 * 1000;
 
 interface MusicResponse<T> {
-	message: {
-		header: {
-			status_code: number;
-			execute_time: number;
-		};
+    message: {
+        header: {
+            status_code: number;
+            execute_time: number;
+        };
 
-		body: T;
-	};
+        body: T;
+    };
 }
 
 class MusicService {
-	accessToken: {
-		access_token: string;
-		token_type: string;
-		expires_in: number;
-		stamp: number;
-	};
+    accessToken: {
+        access_token: string;
+        token_type: string;
+        expires_in: number;
+        stamp: number;
+    };
 
-	cache: { [key: string]: { expiry: number; data: any } } = {};
+    cache: { [key: string]: { expiry: number; data: any } } = {};
 
-	get authToken() {
-		return `${this.accessToken.token_type} ${this.accessToken.access_token}`;
-	}
+    get authToken() {
+        return `${this.accessToken.token_type} ${this.accessToken.access_token}`;
+    }
 
-	async authenticate() {
-		const body = {
-			grant_type: "client_credentials",
-		};
+    async authenticate() {
+        const body = {
+            grant_type: "client_credentials",
+        };
 
-		const response = await fetch(`https://accounts.spotify.com/api/token`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded",
-				Authorization:
-					"Basic " +
-					Buffer.from(
-						process.env.SPOTIFY_CLIENT_ID +
-							":" +
-							process.env.SPOTIFY_CLIENT_SECRET
-					).toString("base64"),
-			},
+        const response = await fetch(`https://accounts.spotify.com/api/token`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization:
+                    "Basic " +
+                    Buffer.from(
+                        process.env.SPOTIFY_CLIENT_ID +
+                            ":" +
+                            process.env.SPOTIFY_CLIENT_SECRET
+                    ).toString("base64"),
+            },
 
-			body: Object.keys(body)
-				.map(
-					(key) =>
-						encodeURIComponent(key) +
-						"=" +
-						encodeURIComponent(body[key])
-				)
-				.join("&"),
-		});
+            body: Object.keys(body)
+                .map(
+                    (key) =>
+                        encodeURIComponent(key) +
+                        "=" +
+                        encodeURIComponent(body[key])
+                )
+                .join("&"),
+        });
 
-		const data = await response.json();
+        const data = await response.json();
 
-		data.stamp = Date.now() + data.expires_in;
+        data.stamp = Date.now() + data.expires_in;
 
-		this.accessToken = data;
-	}
+        this.accessToken = data;
+    }
 
-	async request(type: "Spotify" | "Music", queries: string) {
-		const url = `${
-			type === "Spotify" ? SPOTIFY_API_BASE : MUSIC_API_BASE
-		}${queries}`;
+    async request(type: "Spotify" | "Music", queries: string) {
+        const url = `${
+            type === "Spotify" ? SPOTIFY_API_BASE : MUSIC_API_BASE
+        }${queries}`;
 
-		if (
-			this.cache[url] === undefined ||
-			this.cache[url]?.expiry < Date.now()
-		) {
-			let response: any;
+        if (
+            this.cache[url] === undefined ||
+            this.cache[url]?.expiry < Date.now()
+        ) {
+            let response: any;
 
-			if (type === "Spotify") {
-				if (
-					this.accessToken === undefined ||
-					this.accessToken?.stamp < Date.now()
-				) {
-					await this.authenticate();
-				}
+            if (type === "Spotify") {
+                if (
+                    this.accessToken === undefined ||
+                    this.accessToken?.stamp < Date.now()
+                ) {
+                    await this.authenticate();
+                }
 
-				response = await (
-					await fetch(url, {
-						headers: {
-							"Content-Type": "application/json",
-							Authorization: this.authToken,
-						},
-					})
-				).json();
-			} else if (type === "Music") {
-				response = await (
-					await fetch(`${url}&apikey=${process.env.MUSIC_API_KEY}`)
-				).json();
-			}
+                response = await (
+                    await fetch(url, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: this.authToken,
+                        },
+                    })
+                ).json();
+            } else if (type === "Music") {
+                response = await (
+                    await fetch(`${url}&apikey=${process.env.MUSIC_API_KEY}`)
+                ).json();
+            }
 
-			this.cache[url] = {
-				expiry: Date.now() + CACHE_EXPIRY,
-				data: response,
-			};
-		}
+            this.cache[url] = {
+                expiry: Date.now() + CACHE_EXPIRY,
+                data: response,
+            };
+        }
 
-		return this.cache[url].data;
-	}
+        return this.cache[url].data;
+    }
 
-	async search(
-		query: string = "",
-		type: "album" | "artist" | "track" = "artist",
-		offset: number = 0,
-		limit: number = 25
-	) {
-		const response = await this.request(
-			"Spotify",
-			`/search?q=${encodeURIComponent(
-				query
-			)}&type=${type}&offset=${offset}&limit=${limit}&market=US`
-		);
+    async search(
+        query = "",
+        type: "album" | "artist" | "track" = "artist",
+        offset = 0,
+        limit = 25
+    ) {
+        const response = await this.request(
+            "Spotify",
+            `/search?q=${encodeURIComponent(
+                query
+            )}&type=${type}&offset=${offset}&limit=${limit}&market=US`
+        );
 
-		if (type === "artist") {
-			return response as SpotifyArtistSearch;
-		} else if (type === "track") {
-			return response as SpotifyTrackSearch;
-		}
-	}
+        if (type === "artist") {
+            return response as SpotifyArtistSearch;
+        } else if (type === "track") {
+            return response as SpotifyTrackSearch;
+        }
+    }
 
-	async getPlaylist(playlistId: string) {
-		return (await this.request(
-			"Spotify",
-			`/playlists/${playlistId}`
-		)) as SpotifyPlaylist;
-	}
+    async getPlaylist(playlistId: string) {
+        return (await this.request(
+            "Spotify",
+            `/playlists/${playlistId}`
+        )) as SpotifyPlaylist;
+    }
 
-	async getTracks(artistId: string) {
-		return (await this.request(
-			"Spotify",
-			`/artists/${artistId}/top-tracks?market=us`
-		)) as SpotifyTopTracks;
-	}
+    async getTracks(artistId: string) {
+        return (await this.request(
+            "Spotify",
+            `/artists/${artistId}/top-tracks?market=us`
+        )) as SpotifyTopTracks;
+    }
 
-	async getLyrics(track: SpotifyTrack) {
-		const musicTrack = (await this.request(
-			"Music",
-			`/track.get?track_isrc=${track.external_ids.isrc}`
-		)) as MusicResponse<{ track: MusicTrack }>;
+    async getLyrics(track: SpotifyTrack) {
+        const musicTrack = (await this.request(
+            "Music",
+            `/track.get?track_isrc=${track.external_ids.isrc}`
+        )) as MusicResponse<{ track: MusicTrack }>;
 
-		if (musicTrack.message.body.track.has_lyrics === 0) {
-			return false;
-		}
+        const hasLyrics = musicTrack.message?.body?.track?.has_lyrics;
 
-		return (
-			(await this.request(
-				"Music",
-				`/track.snippet.get?track_id=${musicTrack.message.body.track.track_id}`
-			)) as MusicResponse<MusicLyrics>
-		).message.body.snippet.snippet_body;
-	}
+        if (hasLyrics === undefined || hasLyrics === 0) {
+            return false;
+        }
+
+        return (
+            (await this.request(
+                "Music",
+                `/track.snippet.get?track_id=${musicTrack.message.body.track.track_id}`
+            )) as MusicResponse<MusicLyrics>
+        ).message.body.snippet.snippet_body;
+    }
 }
 
 export default new MusicService();
