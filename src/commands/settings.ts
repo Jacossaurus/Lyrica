b161b2c7be7b6f3b44f37dda21d85ab519bdcc23
Jacossaurus <kingjacob11@icloud.com>
2023-01-08 22:59:53 -0600

    disabe settings fix

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,101 +1,101 @@
 import {
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonInteraction,
-	ButtonStyle,
-	ChatInputCommandInteraction,
-	PermissionFlagsBits,
-	SlashCommandBuilder,
-	StringSelectMenuBuilder,
-	StringSelectMenuInteraction,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    ChatInputCommandInteraction,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+    StringSelectMenuBuilder,
+    StringSelectMenuInteraction,
 } from "discord.js";
 import { deleteReply } from "../utils/deleteReply";
 
 export default {
-	data: new SlashCommandBuilder()
-		.setName("settings")
-		.setDescription("Change Lyrica's settings.")
-		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    data: new SlashCommandBuilder()
+        .setName("settings")
+        .setDescription("Change Lyrica's settings.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
-	async execute(interaction: ChatInputCommandInteraction) {
-		const row =
-			new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-				new StringSelectMenuBuilder()
-					.setCustomId("settingSelect")
-					.setPlaceholder("No setting selected.")
-					.setMinValues(1)
-					.setMaxValues(1)
-					.addOptions([
-						{
-							label: "Explicit Lyrics",
-							description:
-								"If enabled, explicit lyrics may be sent by Lyrica.",
-							value: "explicitLyrics",
-						},
-					])
-			);
+    async execute(interaction: ChatInputCommandInteraction) {
+        const row =
+            new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                new StringSelectMenuBuilder()
+                    .setCustomId("settingSelect")
+                    .setPlaceholder("No setting selected.")
+                    .setMinValues(1)
+                    .setMaxValues(1)
+                    .addOptions([
+                        {
+                            label: "Explicit Lyrics",
+                            description:
+                                "If enabled, explicit lyrics may be sent by Lyrica.",
+                            value: "explicitLyrics",
+                        },
+                    ])
+            );
 
-		await interaction.reply({
-			content: "Please select a setting you would like to change.",
-			components: [row],
-			ephemeral: true,
-		});
-	},
+        await interaction.reply({
+            content: "Please select a setting you would like to change.",
+            components: [row],
+            ephemeral: true,
+        });
+    },
 
-	interactions: {
-		async settingSelect(interaction: StringSelectMenuInteraction) {
-			const selected = interaction.values[0];
+    interactions: {
+        async settingSelect(interaction: StringSelectMenuInteraction) {
+            const selected = interaction.values[0];
 
-			if (selected === "explicitLyrics") {
-				const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-					new ButtonBuilder()
-						.setCustomId("enableExplicitLyrics")
-						.setLabel("Enable")
-						.setStyle(ButtonStyle.Primary),
-					new ButtonBuilder()
-						.setCustomId("disableExplicitLyrics")
-						.setLabel("Disable")
-						.setStyle(ButtonStyle.Danger)
-				);
+            if (selected === "explicitLyrics") {
+                const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    new ButtonBuilder()
+                        .setCustomId("enableExplicitLyrics")
+                        .setLabel("Enable")
+                        .setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder()
+                        .setCustomId("disableExplicitLyrics")
+                        .setLabel("Disable")
+                        .setStyle(ButtonStyle.Danger)
+                );
 
-				await interaction.update({
-					content:
-						"Please choose whether to enable or disable Explicit Lyrics.",
-					components: [row],
-				});
-			} else {
-				await interaction.update({
-					content: "Failed to get setting interaction callback.",
-					components: [],
-				});
-			}
-		},
+                await interaction.update({
+                    content:
+                        "Please choose whether to enable or disable Explicit Lyrics.",
+                    components: [row],
+                });
+            } else {
+                await interaction.update({
+                    content: "Failed to get setting interaction callback.",
+                    components: [],
+                });
+            }
+        },
 
-		async enableExplicitLyrics(interaction: ButtonInteraction) {
-			await interaction.update({
-				content:
-					"Explicit Lyrics has been enabled! Lyrica can now say fuck. :)",
-				components: [],
-			});
+        async enableExplicitLyrics(interaction: ButtonInteraction) {
+            await interaction.update({
+                content:
+                    "Explicit Lyrics has been enabled! Lyrica can now say fuck. :)",
+                components: [],
+            });
 
-			deleteReply(interaction);
-		},
+            deleteReply(interaction);
+        },
 
-		async disableExplicitLyrics(interaction: ButtonInteraction) {
-			await interaction.update({
-				content:
-					"Explicit Lyrics has been disabled! Lyrica cannot say no-no words. :(",
-				components: [],
-			});
+        async disableExplicitLyrics(interaction: ButtonInteraction) {
+            await interaction.update({
+                content:
+                    "Explicit Lyrics has been disabled! Lyrica cannot say no-no words. :(",
+                components: [],
+            });
 
-			setTimeout(() => {
-				interaction.update({
-					content: "Sike- I can still say fuck.",
-					components: [],
-				});
+            setTimeout(async () => {
+                await interaction.editReply({
+                    content: "Sike- I can still say fuck.",
+                    components: [],
+                });
 
-				deleteReply(interaction);
-			}, 2000);
-		},
-	},
+                deleteReply(interaction);
+            }, 3000);
+        },
+    },
 };
